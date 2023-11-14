0cf030aec192453c684f1c1422d68527e3988cd8
MeluFernandez <melufernandez1@gmail.com>
2023-11-14 12:07:54 +0100

    20231114

---
+++ proyectocolegio/includes/funcionesprocole.php
@@ -0,0 +1,39 @@
+<?php
+
+// PASOS PARA CONECTARSE A UNA BASE DE DATOS
+// 1- REALIZAR CONEXIÓN MSQLI_CONNECT EN OTRO ARCHIVO
+// 2- IMPORTAR CREDENCIALES
+// 3- CONSULTA SQL
+// 4- REALIZAR CONSULTA
+// 5- TRABAJAR CON RESULTADOS
+// 6- CERRAR LA CONEXION (OPCIONAL)
+
+function obtener_alumnos() {
+    try {
+        // Importar las credenciales
+        require 'databaseprecole.php';
+
+        // Consulta SQL
+        $sql = "SELECT * FROM colegio LIMIT 5;"; //SIEMPRE DENTRO DE " "
+
+        // Realizar la consulta
+        $consulta = mysqli_query($db, $sql); //LE PASO LA CONEXION Y LA CONSULTA
+
+        // Trabajar con resultados
+        // echo "<pre>";
+        // var_dump(mysqli_fetch_assoc($consulta)); //SOLO ME DEVUELVE EL PRIMERO
+        // echo "</pre>";
+
+        // LO USAREMOS DIRECTAMENTE EN EL HTML
+
+        // Cerrar la conexion (opcional)
+        mysqli_close($db);
+        // PHP CIERRA LA CONEXION DE MANERA AUTOMATICA AL LLEGAR AL FINAL DE ESTE ARCHIVO
+
+        return $consulta;
+    } catch (\Throwable $th) {
+        var_dump($th);
+    }
+}
+
+obtener_alumnos();
